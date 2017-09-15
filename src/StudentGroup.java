f407db380ea118b4bd286804bbed880ebd922a46
dhanunjayavarma <dhanunjayavarmak@gmail.com>
2017-09-15 21:53:42 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -14,16 +14,22 @@ import java.util.*;
 public class StudentGroup implements StudentArrayOperation {
 
 	private Student[] students;
-	public static ArrayList<Student> StudentList=new ArrayList<Student>();
+	//public static ;
 	/**
 	 * DO NOT remove or change this constructor, it will be used during task check
 	 * @param length
 	 */
+	public static ArrayList<Student> StudentList=new ArrayList<Student>();
 	public StudentGroup(int length) {
 		this.students = new Student[length];
 	}
-
+	//completed
+	public int getLength()
+	{
+		return this.students.length;
+	}
 	@Override
+	//completed
 	public Student[] getStudents() {
 		// Add your implementation here
 		this.students = new Student[StudentList.size()];
@@ -32,6 +38,7 @@ public class StudentGroup implements StudentArrayOperation {
 	}
 
 	@Override
+	//completed
 	public void setStudents(Student[] students) {
 		// Add your implementation here
 		int len=this.students.length;
@@ -53,9 +60,10 @@ public class StudentGroup implements StudentArrayOperation {
 	}
 
 	@Override
+	//completed
 	public Student getStudent(int index) {
 		// Add your implementation here
-		int len=this.students.length;
+		int len=getLength();
 		if(index<0||index>=len)
 		{
 			throw new IllegalArgumentException();
@@ -67,6 +75,7 @@ public class StudentGroup implements StudentArrayOperation {
 	}
 
 	@Override
+	//completed
 	public void setStudent(Student student, int index) {
 		// Add your implementation here
 		//System.out.println(student.getFullName());
@@ -85,7 +94,7 @@ public class StudentGroup implements StudentArrayOperation {
 	}
 
 	@Override
-		public void addFirst(Student student) {
+	public void addFirst(Student student) {
 		// Add your implementation here
 		if(student==null)
 		{
@@ -98,7 +107,8 @@ public class StudentGroup implements StudentArrayOperation {
 			toarray(this.students);
 		}
 	}
-@Override
+
+	@Override
 	public void addLast(Student student) {
 		// Add your implementation here
 		if(student==null)
@@ -295,18 +305,37 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void bubbleSort() {
 		// Add your implementation here
+		int i,j;
+		int len=StudentList.size();
+		Student st;
+		for(i=0;i<len-1;i++)
+		{
+			for(j=0;j<len-i-1;j++)
+			{
+				//System.out.println(this.students[j].compareTo(this.students[j+1]));
+				if(StudentList.get(j).compareTo(StudentList.get(j+1))>0)//f>s +
+				{
+					st=this.students[j];
+					StudentList.set(j,this.students[j+1]);
+					StudentList.set(j+1,st);
+				}
+			}
+
+		}
+		this.students = new Student[StudentList.size()];
+		toarray(this.students);
 	}
 
 	@Override
 	public Student[] getByBirthDate(Date date) {
 		// Add your implementation here
 		//Student[] dateStudent;
-		 ArrayList<Student> al=new ArrayList<Student>();
+		public static ArrayList<Student> al=new ArrayList<Student>();
 		int len=StudentList.size();
 		int i;
 		for(i=0;i<len;i++)
 		{
-			if(date.before(StudentList.get(i).getBirthDate())||date.equals(StudentList.get(i).getBirthDate() ))
+			if(date.after(StudentList.get(i).getBirthDate())||date.equals(StudentList.get(i).getBirthDate() ))
 			{
 				//System.out.println(StudentList.get(i).getBirthDate());
 				al.add(this.students[i]);
@@ -326,16 +355,38 @@ public class StudentGroup implements StudentArrayOperation {
 		}
 		return st;
 	}
+
 	@Override
 	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
 		// Add your implementation here
-		return null;
+		Student[] dateStudent;
+		ArrayList<Student> al=new ArrayList<Student>();
+		int len=StudentList.size();
+		int i;
+		for(i=0;i<len;i++)
+		{
+			if( (firstDate.before(StudentList.get(i).getBirthDate())&&lastDate.after(StudentList.get(i).getBirthDate())) ||firstDate.equals(StudentList.get(i).getBirthDate() )||lastDate.equals(StudentList.get(i).getBirthDate()))
+			{
+				//System.out.println(StudentList.get(i).getBirthDate());
+				al.add(this.students[i]);
+			}
+		}
+		dateStudent=new Student[al.size()];
+		dateStudent=toa(al,dateStudent);
+		return dateStudent;
+		//return null;
 	}
 
 	@Override
 	public Student[] getNearBirthDate(Date date, int days) {
 		// Add your implementation here
-		return null;
+		//Date date2=new Date();
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		c.add(Calendar.DATE,days);
+		Date date2=c.getTime();
+		return getBetweenBirthDates(date,date2);
+		//return null;
 	}
 
 	@Override
@@ -353,12 +404,49 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getStudentsWithMaxAvgMark() {
 		// Add your implementation here
-		return null;
+		Double max=Double.MIN_VALUE;
+		int i;
+		int len=this.students.length;
+		for(i=0;i<len;i++)
+		{
+			if(this.students[i].getAvgMark()>max)
+			{
+				max=this.students[i].getAvgMark();
+			}
+		}
+		ArrayList<Student> al=new ArrayList<Student>();
+		for(i=0;i<len;i++)
+		{
+			if(this.students[i].getAvgMark()==max)
+			{
+				al.add(students[i]);
+			}
+		}
+		Student [] dateStudent=new Student[al.size()];
+		dateStudent=toa(al,dateStudent);
+		return dateStudent;
 	}
 
 	@Override
 	public Student getNextStudent(Student student) {
 		// Add your implementation here
-		return null;
+		if(student==null)
+		{
+			throw new IllegalArgumentException();
+		}
+		else
+		{
+			int len=StudentList.size();
+			int i,j;
+			for(i=0;i<len;i++)
+			{
+				if(this.students[i].equals(student))
+				{
+					break;
+				}
+			}
+			return this.students[i+1];	
+		}
+		
 	}
 }
